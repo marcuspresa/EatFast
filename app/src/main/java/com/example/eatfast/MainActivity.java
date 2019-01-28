751430e03579116bbf2e5d9b0db5793d5b932b82
marcuspresa <mackanpk@hotmail.com>
2019-01-28 17:43:13 +0100

    Started with layout

--- app/src/main/java/com/example/eatfast/MainActivity.java
+++ app/src/main/java/com/example/eatfast/MainActivity.java
@@ -1,12 +1,18 @@
 package com.example.eatfast;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
     protected void onCreate(@Nullable Bundle savedInstanceState){
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+    public void navigateToMenu(View view){
+        Intent intent = new Intent(this, MenuActivity.class);
+        startActivity(intent);
     }
 }
