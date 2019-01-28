77608f7a31c0ac79cd6b5fee81f7e72c19f0d0f3
marcuspresa <mackanpk@hotmail.com>
2019-01-28 10:26:58 +0100

    Initial commit

---
+++ app/src/main/java/com/example/eatfast/MainActivity.java
@@ -0,0 +1,12 @@
+package com.example.eatfast;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+    protected void onCreate(@Nullable Bundle savedInstanceState){
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
