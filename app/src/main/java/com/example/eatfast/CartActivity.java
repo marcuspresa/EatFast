70910a0293ba7921ba364a320ac1357e133dc2a7
marcuspresa <mackanpk@hotmail.com>
2019-01-30 16:58:32 +0100

    Added actionbutton

--- app/src/main/java/com/example/eatfast/CartActivity.java
+++ app/src/main/java/com/example/eatfast/CartActivity.java
@@ -1,8 +1,11 @@
 package com.example.eatfast;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
+import android.view.Menu;
+import android.view.MenuItem;
 
 public class CartActivity extends AppCompatActivity {
 
@@ -10,4 +13,12 @@ public class CartActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_cart);
     }
+
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu, menu);
+        return true;
+    }
+
+
 }
