7c67f3040777d2bb7182006530a9b4a55e3a3b8f
marcuspresa <mackanpk@hotmail.com>
2019-02-07 14:53:34 +0100

    Added database

--- app/src/main/java/com/example/eatfast/MainActivity.java
+++ app/src/main/java/com/example/eatfast/MainActivity.java
@@ -1,12 +1,17 @@
 package com.example.eatfast;
 
+import android.content.Context;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
+
+import com.example.eatfast.Database.Database;
 
 public class MainActivity extends AppCompatActivity {
     protected void onCreate(@Nullable Bundle savedInstanceState){
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
+
 }
