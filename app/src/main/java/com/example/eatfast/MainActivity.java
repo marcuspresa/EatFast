190174998ede92cd52f56c3d4a0d782566b9cf83
Marcus Presa Käld <marcuspresakald@gmail.com>
2019-02-25 18:03:46 +0100

    fixed unique id

--- app/src/main/java/com/example/eatfast/MainActivity.java
+++ app/src/main/java/com/example/eatfast/MainActivity.java
@@ -1,16 +1,31 @@
 package com.example.eatfast;
 
+import android.accessibilityservice.AccessibilityService;
 import android.content.Intent;
 import android.content.Context;
+import android.content.SharedPreferences;
+import android.net.wifi.WifiManager;
 import android.os.Bundle;
+import android.provider.Settings;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
+import android.telephony.TelephonyManager;
 import android.view.View;
 
+import com.example.eatfast.Model.User;
+import com.google.gson.Gson;
+
+import java.util.UUID;
+
 public class MainActivity extends AppCompatActivity {
+
     protected void onCreate(@Nullable Bundle savedInstanceState){
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        SharedPreferences mPrefs = getSharedPreferences("UserID",MODE_PRIVATE);
+        SharedPreferences.Editor prefsEditor = mPrefs.edit();
+        prefsEditor.putString("user", UUID.randomUUID().toString());
+        prefsEditor.commit();
 
     }
     public void navigateToMenu(View view){
