8880033e687f431e0cc4252ce5117a4151103e4b
Kevin <berg._95@hotmail.com>
2019-03-09 15:19:28 +0100

    Login Successful

--- app/src/main/java/com/example/eatfast/MainActivity.java
+++ app/src/main/java/com/example/eatfast/MainActivity.java
@@ -19,7 +19,7 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void ordersClicked(View view){
-        Intent intent = new Intent(this, OrderActivity.class);
+        Intent intent = new Intent(this, LoginActivity.class);
         startActivity(intent);
     }
 }
