235c1481177fcfefd602cb1217b28cd73945b75f
Kevin <berg._95@hotmail.com>
2019-03-15 16:22:30 +0100

    Payment Done

--- app/src/main/java/com/example/eatfast/OrderActivity.java
+++ app/src/main/java/com/example/eatfast/OrderActivity.java
@@ -1,5 +1,7 @@
 package com.example.eatfast;
 
+import android.content.Intent;
+import android.os.Handler;
 import android.support.design.widget.TabLayout;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
@@ -19,6 +21,7 @@ import android.view.View;
 import android.view.ViewGroup;
 
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class OrderActivity extends AppCompatActivity {
 
@@ -49,6 +52,20 @@ public class OrderActivity extends AppCompatActivity {
         adapter.addFragment(new FragOrders(), "Pending orders");
         adapter.addFragment(new FragDoneOrders(), "Completed orders");
         viewPager.setAdapter(adapter);
+    }
+
+    public void signOutbtn(View view){
+        Toast.makeText(OrderActivity.this, "Signing Out",
+                Toast.LENGTH_SHORT).show();
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent = new Intent(OrderActivity.this, MainActivity.class);
+                startActivity(intent);
+            }
+        },1500);
+
     }
 
 
--- app/src/main/java/com/example/eatfast/paymentActivity.java
+++ app/src/main/java/com/example/eatfast/paymentActivity.java
@@ -1,27 +1,75 @@
 package com.example.eatfast;
 
+
+import android.content.DialogInterface;
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
+import android.text.InputType;
+import android.view.InputDevice;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
+
+import com.braintreepayments.cardform.view.CardForm;
 
 public class paymentActivity extends AppCompatActivity {
 
+    CardForm cardForm;
+    Button buy;
+    AlertDialog.Builder alertBuilder;
+
+    @Override
     protected void onCreate(@Nullable Bundle savedInstanceState){
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_payment);
 
-        Button button = (Button) findViewById(R.id.payButton);
-
-        button.setOnClickListener(new View.OnClickListener() {
+        cardForm = findViewById(R.id.card_form);
+        buy = findViewById(R.id.btnBuy);
+        cardForm.cardRequired(true)
+                .expirationRequired(true)
+                .cvvRequired(true)
+                .postalCodeRequired(true)
+                .mobileNumberExplanation("SMS is required on this number")
+                .mobileNumberRequired(true)
+                .setup(paymentActivity.this);
+        cardForm.getCvvEditText().setInputType(InputType.TYPE_CLASS_NUMBER | InputType.TYPE_NUMBER_VARIATION_PASSWORD);
+        buy.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View v) {
-
-                System.out.println("PAYED");
-
+            public void onClick(View view) {
+                if (cardForm.isValid()) {
+                    AlertDialog.Builder alertBuilder = new AlertDialog.Builder(paymentActivity.this);
+                    alertBuilder.setTitle("Confirm before purchase");
+                    alertBuilder.setMessage("Card number: " + cardForm.getCardNumber() + "\n" +
+                            "Card expiry date: " + cardForm.getExpirationDateEditText().getText().toString() + "\n" +
+                            "Card CVV: " + cardForm.getCvv() + "\n" +
+                            "Postal code: " + cardForm.getPostalCode() + "\n" +
+                            "Phone number: " + cardForm.getMobileNumber());
+                    alertBuilder.setPositiveButton("Confirm", new DialogInterface.OnClickListener() {
+                        @Override
+                        public void onClick(DialogInterface dialogInterface, int i) {
+                            dialogInterface.dismiss();
+                            Toast.makeText(paymentActivity.this, "Thank you for purchase", Toast.LENGTH_LONG).show();
+                        }
+                    });
+                    alertBuilder.setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
+                        @Override
+                        public void onClick(DialogInterface dialogInterface, int i) {
+                            dialogInterface.dismiss();
+                        }
+                    });
+                    AlertDialog alertDialog = alertBuilder.create();
+                    alertDialog.show();
+                } else {
+                    Toast.makeText(paymentActivity.this, "Please complete the form", Toast.LENGTH_LONG).show();
+                }
             }
-            }
-        );
+        });
+
+
     }
+
+
 }
