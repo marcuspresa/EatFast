c052536930e4f78cb642062fb4640c65f73af7b9
Kevin <berg._95@hotmail.com>
2019-03-15 13:38:02 +0100

    Payment Update

--- app/src/main/java/com/example/eatfast/paymentActivity.java
+++ app/src/main/java/com/example/eatfast/paymentActivity.java
@@ -1,5 +1,6 @@
 package com.example.eatfast;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
@@ -8,20 +9,13 @@ import android.widget.Button;
 
 public class paymentActivity extends AppCompatActivity {
 
-    protected void onCreate(@Nullable Bundle savedInstanceState){
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_payment);
+    }
 
-        Button button = (Button) findViewById(R.id.payButton);
-
-        button.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-
-                System.out.println("PAYED");
-
-            }
-            }
-        );
+    public void payBtnClicked(View view){
+        Intent intent = new Intent(this, CreditCardPayment.class);
+        startActivity(intent);
     }
 }
